f02822fc380474e05aaa455ab4bc55ea6fbc66a7
Ruslan Rudiuk <rus.barik@gmail.com>
2024-10-15 16:57:34 +0300

    Update files to link them

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 @app.route('/data')
 def get_data():
     data = {
-        "Version_int": 2,
-        "Version": "2.0.1"
+        "Version_int": 3.8,
+        "Version": "3.8"
     }
     return jsonify(data)
 
